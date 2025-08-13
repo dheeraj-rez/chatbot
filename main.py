dcdc0394fd5cd12292b605146ba197586bb7c19d
durgesh-rez <durgesh.mahato@rezolut.in>
2025-08-13 17:14:39 +0530

    add CORS setup in api for all

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 # main.py
 from fastapi import FastAPI
 from api.chatbot import router as chatbot_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="LangChain Loan Bot API (Production)",
     description="A structured API for a loan assistance chatbot powered by LangChain."
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # The list of origins that are allowed to make cross-origin requests.
+    allow_credentials=True, # Allows cookies to be included in requests.
+    allow_methods=["*"],    # Allows all methods (GET, POST, etc.).
+    allow_headers=["*"],    # Allows all headers.
 )
 
 # Include the chatbot router with a prefix for versioning, e.g., /api/v1/chat
